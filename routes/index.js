56309275437f9eae0b712c9b2ef9b5cd4af3db92
hwakabh <hrykwkbys1024@gmail.com>
2021-02-17 07:42:19 +0900

    Added Cloud Build config for using secure environmental vars with berglas.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 var express = require('express');
+const { route } = require('../app');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  //// Fetch environmental variable securely
+  // let node_secret = process.env.NODE_SECRET;
+  // console.log(`Fetched value from env: ${node_secret}`);
+
+  // If you'd like to pass value for views, use `res.render('VIEW_NAME', {SECRET_VAR : node_secret})`
   res.render('home');
 });
 
