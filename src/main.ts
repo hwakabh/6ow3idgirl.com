cab0036f510d633292d0a7776a30e8f3f7e21477
hwakabh <hrykwkbys1024@gmail.com>
2024-03-08 22:09:48 +0900

    feat!: overlayed Nest with TypeScript on current Express.js app.

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  // https://docs.nestjs.com/first-steps#platform
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // const app = await NestFactory.create(AppModule);
+
+  // https://docs.nestjs.com/techniques/mvc
+  // https://github.com/nestjs/nest/tree/master/sample/15-mvc
+  // __dirname returns 'PROJECT_ROOT/dist'
+  app.useStaticAssets(join(__dirname, '..', '/src/public'));
+  app.setBaseViewsDir(join(__dirname, '..', '/src/views'));
+  app.setViewEngine('pug');
+
+  await app.listen(3000);
+}
+bootstrap();
