f1d8f8dfc576a2ec25ca6665ea8772bf143f0ab8
hwakabh <hrykwkbys1024@gmail.com>
2024-03-08 23:54:10 +0900

    deps(types): added types of middlewares by express-generator.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+
+// Express general
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { Request, Response, NextFunction } from "express";
+
+// middlewares by express-generator
+// import createHttpError from "http-errors";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
 
 async function bootstrap() {
   // https://docs.nestjs.com/first-steps#platform
@@ -14,6 +22,25 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', '/src/public'));
   app.setBaseViewsDir(join(__dirname, '..', '/src/views'));
   app.setViewEngine('pug');
+
+  app.use(cookieParser());
+  app.use(logger("dev"));
+
+  // TODO: should be replaced with helmet
+  app.disable('x-powered-by');
+
+  // TODO: fix 404 with import controllers(routers)
+  // // Error handler
+  // app.use((req: Request, res: Response, next: NextFunction) =>
+  //   next(createHttpError(404))
+  // );
+  // app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  //   res.locals.message = err.message;
+  //   res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  //   res.status(err.status || 500);
+  //   res.render("error");
+  // });
 
   await app.listen(8080);
 }
