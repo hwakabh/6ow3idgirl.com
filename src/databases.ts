7dc7ab1ace5fbd9bcc1847e8fcc27703d143efe9
hwakabh <hrykwkbys1024@gmail.com>
2025-01-16 22:49:07 +0900

    feat: added default values of database credentials.

--- src/databases.ts
+++ src/databases.ts
@@ -2,11 +2,11 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: process.env.MYSQL_HOSTNAME,
-  port: Number(process.env.MYSQL_PORT),
-  database: process.env.MYSQL_DATABASE,
-  username: process.env.MYSQL_USERNAME,
-  password: process.env.MYSQL_PASSWORD,
+  host: process.env.MYSQL_HOSTNAME ?? '0.0.0.0',
+  port: Number(process.env.MYSQL_PORT) ?? 3306,
+  database: process.env.MYSQL_DATABASE ?? 'roza',
+  username: process.env.MYSQL_USERNAME ?? 'root',
+  password: process.env.MYSQL_PASSWORD ?? 'root',
   entities: ['src/entities/*.ts'],
   migrations: ['src/migrations/*.ts'],
   logging: true,
