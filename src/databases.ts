650f17334b64b5dae5a22b1a50bef11958d26a6c
hwakabh <hrykwkbys1024@gmail.com>
2025-01-29 18:04:01 +0900

    feat(typeorm): enabled to parse credentials from JAWSDB_URL.

--- src/databases.ts
+++ src/databases.ts
@@ -2,12 +2,12 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: process.env.MYSQL_HOSTNAME ?? '0.0.0.0',
-  port: Number(process.env.MYSQL_PORT) ?? 3306,
-  database: process.env.MYSQL_DATABASE ?? 'roza',
-  username: process.env.MYSQL_USERNAME ?? 'root',
-  password: process.env.MYSQL_PASSWORD ?? 'root',
-  entities: ['src/entities/*.ts'],
+  host: process.env.JAWSDB_URL.split(/\/|:|@/)[5] ?? '0.0.0.0',
+  port: Number(process.env.JAWSDB_URL.split(/\/|:|@/)[6]) ?? 3306,
+  database: process.env.JAWSDB_URL.split(/\/|:|@/)[7] ?? 'roza',
+  username: process.env.JAWSDB_URL.split(/\/|:|@/)[3] ?? 'root',
+  password: process.env.JAWSDB_URL.split(/\/|:|@/)[4] ?? 'root',
+entities: ['src/entities/*.ts'],
   migrations: ['src/migrations/*.ts'],
   logging: true,
   // if true, data will be automatically updated with detecting changes
